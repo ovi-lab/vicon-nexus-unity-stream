105338211d821695e0ac984b31d979d7080fe8db
Ahmed Shariff <shariff.mfa@outlook.com>
2024-06-23 22:44:02 -0700

    Editor scripts reflect DataStreamer -> SubjectDataManager refactor

--- Assets/Scripts/ViconNexusUnityStream/Editor/CustomSubjectConfig.cs
+++ Assets/Scripts/ViconNexusUnityStream/Editor/CustomSubjectConfig.cs
@@ -6,11 +6,6 @@ namespace ubco.ovilab.ViconUnityStream
     public class CustomSubjectConfig : ScriptableSingleton<CustomSubjectConfig>
     {
         private bool enabled = true;
-        private bool enableWriteData = true;
-        private bool useDefaultData = false;
-        private bool useJson = true;
-        private bool useXRHandsSubsystem = true;
-        private string baseURI = "http://127.0.0.1:5000/marker/test";
 
         public bool Changed { get; private set; } = false;
 
@@ -24,69 +19,6 @@ namespace ubco.ovilab.ViconUnityStream
                     Changed = true;
                 }
                 enabled = value;
-            }
-        }
-
-        public bool EnableWriteData {
-            get {
-                return enableWriteData;
-            }
-            set {
-                if (enableWriteData != value)
-                {
-                    Changed = true;
-                }
-                enableWriteData = value;
-            }
-        }
-
-        public bool UseDefaultData {
-            get {
-                return useDefaultData;
-            }
-            set {
-                if (useDefaultData != value)
-                {
-                    Changed = true;
-                }
-                useDefaultData = value;
-            }
-        }
-
-        public bool UseJson {
-            get {
-                return useJson;
-            }
-            set {
-                if (useJson != value)
-                {
-                    Changed = true;
-                }
-                useJson = value;
-            }
-        }
-
-        public string URI {
-            get {
-                return baseURI;
-            }
-            set {
-                if (baseURI != value)
-                {
-                    Changed = true;
-                }
-                baseURI = value;
-            }
-        }
-
-        public bool UseXRHandsSubsystem {
-            get => useXRHandsSubsystem;
-            set {
-                if (useXRHandsSubsystem != value)
-                {
-                    Changed = true;
-                }
-                useXRHandsSubsystem = value;
             }
         }
 
--- Assets/Scripts/ViconNexusUnityStream/Editor/CustomSubjectScriptEditor.cs
+++ Assets/Scripts/ViconNexusUnityStream/Editor/CustomSubjectScriptEditor.cs
@@ -1,5 +1,5 @@
-using System.Collections.Generic;
 using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
 
 namespace ubco.ovilab.ViconUnityStream.Editor
@@ -8,89 +8,72 @@ namespace ubco.ovilab.ViconUnityStream.Editor
     [CanEditMultipleObjects]
     public class CustomSubjectScriptEditor: UnityEditor.Editor
     {
-        private static List<CustomSubjectScript> subjectScripts;
+        private static readonly string[] excludedSerializedNames = new string[]{"m_Script", "subjectDataManager"};
         private CustomSubjectScript customSubjectScript;
+
+        private SerializedProperty scriptProperty;
+        private SerializedProperty subjectDataManagerProperty;
 
         private void OnEnable()
         {
-            if (subjectScripts == null)
-            {
-                subjectScripts = new List<CustomSubjectScript>();
-                subjectScripts.AddRange(FindObjectsOfType<CustomSubjectScript>());
-            }
-
             customSubjectScript = target as CustomSubjectScript;
-            if (!subjectScripts.Contains(customSubjectScript))
-            {
-                subjectScripts.Add(customSubjectScript);
-            }
-            CustomSubjectConfig.instance.Enabled = customSubjectScript.enabled;
-            CustomSubjectConfig.instance.UseDefaultData = customSubjectScript.useDefaultData;
-            CustomSubjectConfig.instance.UseJson = customSubjectScript.useJson;
-            CustomSubjectConfig.instance.EnableWriteData = customSubjectScript.enableWriteData;
-            CustomSubjectConfig.instance.URI = customSubjectScript.URI;
-            if (customSubjectScript is CustomHandScript handScript)
-            {
-                CustomSubjectConfig.instance.UseXRHandsSubsystem = handScript.useHandSubsystem;
-            }
 
-            if (CustomSubjectConfig.instance.Changed)
-            {
-                UpdateContent();
-            }
-        }
-
-        private void OnDisable()
-        {
-            subjectScripts = null;
+            scriptProperty = serializedObject.FindProperty("m_Script");
+            subjectDataManagerProperty = serializedObject.FindProperty("subjectDataManager");
         }
 
         public override void OnInspectorGUI()
         {
-            EditorGUILayout.LabelField("<color=#eeeeee>----- Common Config -----</color>", new GUIStyle() { fontStyle = FontStyle.Bold, alignment = TextAnchor.MiddleCenter, richText = true});
-            EditorGUI.BeginChangeCheck();
-            CustomSubjectConfig.instance.URI = EditorGUILayout.TextField("base URI", CustomSubjectConfig.instance.URI);
-            CustomSubjectConfig.instance.Enabled = EditorGUILayout.Toggle("Enabled", CustomSubjectConfig.instance.Enabled);
-            CustomSubjectConfig.instance.UseDefaultData = EditorGUILayout.Toggle("Use default data", CustomSubjectConfig.instance.UseDefaultData);
-            CustomSubjectConfig.instance.UseJson = EditorGUILayout.Toggle("Use json for network", CustomSubjectConfig.instance.UseJson);
-            CustomSubjectConfig.instance.EnableWriteData = EditorGUILayout.Toggle("Write data to file", CustomSubjectConfig.instance.EnableWriteData);
-            CustomSubjectConfig.instance.UseXRHandsSubsystem = EditorGUILayout.Toggle("Use XR Hand subsystem", CustomSubjectConfig.instance.UseXRHandsSubsystem);
-            if (EditorGUI.EndChangeCheck())
+            GUI.enabled = false;
+            EditorGUILayout.PropertyField(scriptProperty);
+            GUI.enabled = true;
+
+            EditorGUILayout.Space();
+
+            EditorGUILayout.PropertyField(subjectDataManagerProperty);
+            SubjectDataManager subjectDataManager = customSubjectScript.subjectDataManager;
+            if (subjectDataManager == null)
             {
-                UpdateContent();
+                string buttonText;
+                subjectDataManager = FindObjectOfType<SubjectDataManager>();
+                if (subjectDataManager != null)
+                {
+                    EditorGUILayout.HelpBox("SubjectDataManager is not assigned. A SubjectDataManager is in the scene", MessageType.Error);
+                    buttonText = "Assign SubjectDataManager";
+                }
+                else
+                {
+                    EditorGUILayout.HelpBox("SubjectDataManager is not assigned.", MessageType.Error);
+                    buttonText = "Assign SubjectDataManager";
+                }
+
+                if (GUILayout.Button(buttonText))
+                {
+                    if (subjectDataManager == null)
+                    {
+                        subjectDataManager = (new GameObject("SubjectDataManager")).AddComponent<SubjectDataManager>();
+                    }
+                    customSubjectScript.subjectDataManager = subjectDataManager;
+                    EditorUtility.SetDirty(customSubjectScript);
+                }
+            }
+            else
+            {
+                EditorGUILayout.LabelField("URI", subjectDataManager.BaseURI);
+                EditorGUILayout.LabelField("Use default data", subjectDataManager.UseDefaultData.ToString());
+                EditorGUILayout.LabelField("Enable Write data", subjectDataManager.EnableWriteData.ToString());
+                EditorGUILayout.LabelField("Use XR Hand Subsystem", subjectDataManager.UseXRHandSubsystem.ToString());
+                if (GUILayout.Button("Modify subject data manager"))
+                {
+                    Selection.activeObject = subjectDataManager;
+                    InternalEditorUtility.SetIsInspectorExpanded(subjectDataManager, true);
+                }
             }
 
             EditorGUILayout.Space();
-            EditorGUILayout.LabelField("<color=#eeeeee>----- Component Spcific Config -----</color>", new GUIStyle() { fontStyle = FontStyle.Bold, alignment = TextAnchor.MiddleCenter, richText = true});
-            base.DrawDefaultInspector();
-        }
 
-        private void UpdateContent()
-        {
-            CustomSubjectConfig.instance.Save();
-            foreach (CustomSubjectScript script in subjectScripts)
-            {
-                script.enabled = CustomSubjectConfig.instance.Enabled;
-                script.useDefaultData = CustomSubjectConfig.instance.UseDefaultData;
-                script.useJson = CustomSubjectConfig.instance.UseJson;
-                script.enableWriteData = CustomSubjectConfig.instance.EnableWriteData;
-                script.URI = CustomSubjectConfig.instance.URI;
-                if (customSubjectScript is CustomHandScript handScript)
-                {
-                    handScript.useHandSubsystem = CustomSubjectConfig.instance.UseXRHandsSubsystem;
-                }
-
-                EditorUtility.SetDirty(script);
-            }
-            Debug.Log($"Updated {subjectScripts.Count} subject script(s):"+
-                      $"\n    Setting URI:        {customSubjectScript.URI};"+
-                      $"\n    Enabled:            {CustomSubjectConfig.instance.Enabled};"+
-                      $"\n    Using default data: {customSubjectScript.useDefaultData};"+
-                      $"\n    Using json:         {customSubjectScript.useJson};"+
-                      $"\n    Using XRHandSubsys: {CustomSubjectConfig.instance.UseXRHandsSubsystem};"+
-                      $"\n    Writing data:       {customSubjectScript.enableWriteData};"+
-                      $"\n    Scripts updated: \n         " +
-                      string.Join(",\n         ", subjectScripts));
+            DrawPropertiesExcluding(serializedObject, excludedSerializedNames);
+            serializedObject.ApplyModifiedProperties();
         }
     }
 }
